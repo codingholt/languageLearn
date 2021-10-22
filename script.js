f40aa493263d90c8b62911ae0eb0e6b2b54edb4e
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-10-22 14:54:44 +0200

    made it completely work, next more questions

--- script.js
+++ script.js
@@ -3,28 +3,50 @@ let title  = document.getElementById('title');
 let btn = document.getElementById('btn');
 // btn.addEventListener('click', func)
 
-let question  = {
-    word : 'gato',
-    options : ['dog', 'cat', 'crocodile', 'owl'],
-    answer : 1,
-};
+let question = [
+  {
+    word: 'gato',
+    options: ['dog', 'cat', 'bird', 'fish'],
+    answer: 1
+  },
+  {
+    word: 'ave',
+    options: ['mouse', 'hamster', 'lizard', 'bird'],
+    answer: 3
+  },
+  {
+    word: 'rata',
+    options: ['cat', 'fish', 'rat', 'shark'],
+    answer: 2
+  },
+  {
+    word: 'mosca',
+    options: ['fly', 'puma', 'fish', 'dog'],
+    answer: 0
+  }
+];
 
 let app = {
     start : function(){
-        let alts = document.querySelectorAll('.alternative');
+        let opts = document.querySelectorAll('.options');
     
-        alts.forEach(function(element, index){
-          element.addEventListener('click', function(){
+        opts.forEach((element, index) => {
+          element.addEventListener('click', () =>{
             // check correct answer
-            console.log('check correct answer');
+            this.checkAnswer(index)
+            
           });
         });
-      
+          
         // show first question
         this.displayQuestion(question);
     },
 
     displayQuestion : function(q){
+
+        //what question are we currently showing
+        this.currQuestion = q;
+
         let title  = document.getElementById('title');
         title.textContent = q.word;
 
@@ -33,7 +55,16 @@ let app = {
         //display options
         opts.forEach(function(element, index){
             element.textContent = q.options[index];
-        });   
+        });     
+    },
+
+    checkAnswer : function(userSelected){
+        if(this.currQuestion.answer ===  userSelected){
+            console.log('answer: correct')
+        }
+        else{
+            console.log('answer: incorrect')
+        }
     }
 };
 

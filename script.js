ed5cc25cc7b54bf9b065ff7521aa38614a6ae8a0
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-10-23 19:43:41 +0200

    added modal

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ let settings = {
         // Get the modal
       var modal = document.getElementById("myModal");
       // Get the button that opens the modal
-      var mbtn = document.getElementById("myBtn");
+      var mbtn = document.getElementById("addQuestionBtn");
       // Get the <span> element that closes the modal
       var span = document.getElementsByClassName("close")[0];
       // When the user clicks on the button, open the modal
